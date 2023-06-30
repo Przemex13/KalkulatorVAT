0d3d966c705cffee957b5a79d4f282502a460ed8
Przemyslaw Poskrobko <przemyslaw.poskrobko@gmail.com>
2023-06-30 19:39:57 +0200

    Wersja dla Jopy

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
 import java.awt.event.*;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Main extends JFrame{
     private JPanel mainPanel;
@@ -12,29 +10,17 @@ public class Main extends JFrame{
     private JTextField grosValueTextField;
     private JButton obliczButton;
     private JPanel panel;
-
-
     private String netValueDisplayString;
     private String vatTaxDisplayString;
     private String grosValueDisplayString;
-
     public static boolean[]isTexdFieldInUse = new boolean[3];
-
-
-
-
-
-
     public Main (){
         setTitle("Kalkulator VAT");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(400, 230);
         setVisible(true);
-
         setContentPane(mainPanel);
 // listeners
-
-
         netValueTextField.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
@@ -45,7 +31,6 @@ public class Main extends JFrame{
                 }else {
                     e.consume();
                 }
-
                 netValueDisplayString = netValueTextField.getText() + pomocniczyNetValue;
                 if (Double.parseDouble(netValueDisplayString) == 0) isTexdFieldInUse[0] = false;
                 else isTexdFieldInUse[0] = true;
@@ -99,13 +84,10 @@ public class Main extends JFrame{
 
             @Override
             public void focusLost(FocusEvent e) {
-
-
                 if (vatTextField.getText().isEmpty()){
                     vatTaxDisplayString ="0";
                     vatTextField.setText("0.00");
                     if(vatTextField.getText().isEmpty()) vatTextField.setText("0.00");
-
                 }
                 else{
                     vatTextField.setText(String.valueOf(roundNumberTwoDecimals(Double.valueOf(vatTaxDisplayString))));
@@ -119,15 +101,10 @@ public class Main extends JFrame{
                 }
             }
         });
-
-
-
         grosValueTextField.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
-
                 char pomocniczyGrosValue =' ';
-
                 if (isCharValid(e.getKeyChar())&& isContainComa(grosValueTextField, e.getKeyChar())){
                     pomocniczyGrosValue = e.getKeyChar();
                 }else {
@@ -143,7 +120,6 @@ public class Main extends JFrame{
 
             @Override
             public void focusLost(FocusEvent e) {
-
                 if (grosValueTextField.getText().isEmpty()){
                     grosValueDisplayString ="0";
                     grosValueTextField.setText("0.00");
@@ -165,7 +141,6 @@ public class Main extends JFrame{
         obliczButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-
                 System.out.println("===========================================");
                 System.out.println("net value :" + netValueDisplayString);
                 System.out.println("vat value :" + vatTaxDisplayString);
@@ -174,7 +149,6 @@ public class Main extends JFrame{
         });
     }
     private static boolean controlWhichWindowToFreeze(boolean [] tablica){
-
        int trueInt = 0;
        int falseInt = 0;
        int nrPusty = -1;
@@ -183,41 +157,25 @@ public class Main extends JFrame{
            if (tablica[i] == false) falseInt ++;
            if(trueInt == 2){
                nrPusty = Arrays.asList(tablica).indexOf(false);
-
-               System.out.println(nrPusty);
            }
-
        }
-
         return  false;
     }
     private boolean isCharValid (char element){
-
         return (element >= '0' && element <= '9' ||
                 element == '.')
                 ? true : false;
     }
-
-
     private static boolean isContainComa(JTextField pole, char wpis){
-
             boolean isOK = true;
-
             if (pole.getText().indexOf('.') != -1 && wpis == '.') isOK = false;
             if(pole.getText().isEmpty()) isOK = true;
             return isOK ;
-
-
     }
-
-
     private static Double roundNumberTwoDecimals (double number){
         number *= 100;
         number = Math.round(number);
         number /= 100;
-
-
-
         return number;
     }
         public static void main(String[] args) {
